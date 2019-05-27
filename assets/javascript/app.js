c636104aaec4b62aaee751e18be7c13e50248ae0
kmj217 <kmj217@hotmail.com>
2019-05-26 23:29:41 -0400

    created questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+//need arrays for questions to be used
+var questions = [
+    {
+      question: "What is Spiderman's real name?",
+      answers: [
+        { answer: "A. Peter Parker", value: true },
+        { answer: "B. Tony Starker", value: false },
+        { answer: "C. Peter Johnson", value: false },
+        { answer: "D. Miles Parker", value: false }
+      ]
+    },
+    {
+    question: "Who was the first enemy of the Fantastic Four?",
+    answers: [
+        { answer: "A. Dr. Doom", value: true },
+        { answer: "B. Galactus", value: false },
+        { answer: "C. Mole Man", value: false },
+        { answer: "D. Puppet Master", value: false }
+    ]
+    },
+    {
+    question: "Which of the following is NOT a true fact about the Hulk?",
+    answers: [
+        { answer: "A. Very susceptible to mind control", value: true },
+        { answer: "B. Does not need to sleep", value: false },
+        { answer: "C. Immune to all diseases", value: false },
+        { answer: "D. Near impenatrable skin", value: false }
+    ]
+    },
+    {
+    question: "What was the original name of the X-Men?",
+    answers: [
+        { answer: "A. Merry Mutants", value: true },
+        { answer: "B. Project-M", value: false },
+        { answer: "C. Mutant Militia", value: false },
+        { answer: "D. X-Mutants", value: false }
+    ]
+    },
+    {
+    question: "What creatures did the Comics Code Authority ban Marvel from adding into their comics?",
+    answers: [
+        { answer: "A. Werewolves", value: true },
+        { answer: "B. Zombies", value: false },
+        { answer: "C. Vampires", value: false },
+        { answer: "D. Aliens", value: false }
+    ]
+    },
+  ];
+//need a function to start the game when the start button is clicked
+//  This should 
+
+//Set variables for corrects answers, incorrect answers, timer, 
+
+//need a function 
