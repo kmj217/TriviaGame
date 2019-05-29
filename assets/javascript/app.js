47f30db494c341102f36caa135d6bc518b27c9c0
kmj217 <kmj217@hotmail.com>
2019-05-28 20:26:08 -0400

    most things work, need to fix correct answer background color

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,8 +55,99 @@ var correctCounter = 0;
 var incorrectCounter = 0;
 var unansweredCounter = 0;
 
+    // Create function to update the correct answer counter, reset the clock, and add the number of correctly answered questions to the DOM
+    function rightAnswer() {
+        correctCounter++;
+        $('.time').html(timer);
+        $('.right').html('<p>Right answers: ' + correctCounter + '</p>');
+        setTimeout(questionCounter, 1000);
+      }
+  
+      // Create function to update the incorrect answer counter, reset the clock, and add the number of incorrectly answered questions to the DOM
+      function wrongAnswer() {
+          incorrectCounter++;
+          $('.time').html(timer);
+          $('.wrong').html('<p>Wrong answers: ' + incorrectCounter + '</p>');
+          setTimeout(questionCounter, 1000);
+        }
+        
+      // Create function to update the unanswered counter, reset the clock, and add the number of unanswered questions to the DOM
+      function unanswered() {
+          unansweredCounter++;
+          $('.time').html(timer);
+          $('.main').append("<p class='times-up'>Time's up!</p>");
+          $('.first-answer').css('background-color', 'green');
+          $('.unanswered').html('<p>Unanswered Questions: ' + unansweredCounter + '</p>');
+          $('.times-up')
+            .delay(2000)
+            .fadeOut(400);
+          setTimeout(questionCounter, 2000);
+        }
+        
+      // Start the game with a function to display the questions and start the time
+      function startTrivia() {
+          $('.start-page').css('display', 'none');
+          $('.questions-page').css('visibility', 'visible');
+          $('.timer').html('<p>Time remaining: <span class="time">10</span></p>');
+        
+          $('.question').html(questions[counter].question);
+          var displayAnswers =
+            '<p class="answer first-answer">' +
+            questions[counter].answers[0].answer +
+            '</p><p class="answer">' +
+            questions[counter].answers[1].answer +
+            '</p><p class="answer">' +
+            questions[counter].answers[2].answer +
+            '</p><p class="answer">' +
+            questions[counter].answers[3].answer +
+            '</p>';
+        
+          $('.answers').html(displayAnswers);
+        
+          timeHolder();
+        }
+        
+        // Create a function to continue the game as long as there are questions to ask. Finish game when all questions are asked
+        function questionCounter() {
+          if (counter < 4) {
+            counter++;
+            startTrivia();
+            timer = 10;
+            timeHolder();
+          } else {
+            finishTrivia();
+          }
+        }
+        
+        // Timer function
+        function timeHolder() {
+          clearInterval(clock);
+          clock = setInterval(seconds, 1000);
+          function seconds() {
+              if (timer === 0) {
+              clearInterval(clock);
+              unanswered();
+            } else if (timer > 0) {
+              timer--;
+              $('.time').html(timer);
+            }
+          }
+        }
+        
+        // Append reset button, correct, incorrect, and unanswered questions upon completing the trivia
+        function finishTrivia() {
+          var final = $('.main')
+            .html("<p>Here's your results!<p><br>")
+            .append('<p>Correct Answers: ' + correctCounter + '</p><br>')
+            .append('<p>Wrong Answers: ' + incorrectCounter + '</p><br>')
+            .append('<p>Unanswered Answers: ' + unansweredCounter + '</p><br>');
+          $(final).attr('<div>');
+          $(final).attr('class', 'final');
+          $('.final').append('<p><a class="reset-button" href="#">Restart Trivia!</a></p>');
+        }
+
 $(document).ready(function() {
-    // Start the game when that start button is clicked
+    // Start the trivia when that start button is clicked
     $('.answers').css('visibility', 'hidden');
     $('body').on('click', '.start-btn', function() {
       startTrivia();
@@ -85,94 +176,3 @@ $(document).ready(function() {
         location.reload();
       });
     });
-    
-// Create function to update the correct answer counter, reset the clock, and add the number of correctly answered questions to the DOM
-    function rightAnswer() {
-      correctCounter++;
-      $('.time').html(timer);
-      $('.right').html('<p>Right answers: ' + correctCounter + '</p>');
-      setTimeout(questionCounter, 1000);
-    }
-
-// Create function to update the incorrect answer counter, reset the clock, and add the number of incorrectly answered questions to the DOM
-    function wrongAnswer() {
-        incorrectCounter++;
-        $('.time').html(timer);
-        $('.wrong').html('<p>Wrong answers: ' + incorrectCounter + '</p>');
-        setTimeout(questionCounter, 1000);
-      }
-      
-// Create function to update the unanswered counter, reset the clock, and add the number of unanswered questions to the DOM
-      function unanswered() {
-        unansweredCounter++;
-        $('.time').html(timer);
-        $('.main').append("<p class='times-up'>Time's up!</p>");
-        $('.first-answer').css('background-color', 'green');
-        $('.unanswered').html('<p>Unanswered Questions: ' + unansweredCounter + '</p>');
-        $('.times-up')
-          .delay(2000)
-          .fadeOut(400);
-        setTimeout(questionCounter, 2000);
-      }
-      
-      // Start the game with a function to display the questions and start the time
-      function startTrivia() {
-        $('.start-page').css('display', 'none');
-        $('.questions-page').css('visibility', 'visible');
-        $('.timer').html('<p>Time remaining: <span class="time">10</span></p>');
-      
-        $('.question').html(questions[counter].question);
-        var displayAnswers =
-          '<p class="answer first-answer">' +
-          questions[counter].answers[0].answer +
-          '</p><p class="answer">' +
-          questions[counter].answers[1].answer +
-          '</p><p class="answer">' +
-          questions[counter].answers[2].answer +
-          '</p><p class="answer">' +
-          questions[counter].answers[3].answer +
-          '</p>';
-      
-        $('.answers').html(displayAnswers);
-      
-        timeHolder();
-      }
-      
-      // Create a function to continue the game as long as there are questions to ask. Finish game when all questions are asked
-      function questionCounter() {
-        if (counter < 4) {
-          counter++;
-          startTrivia();
-          timer = 10;
-          timeHolder();
-        } else {
-          finishTrivia();
-        }
-      }
-      
-      // Timer function
-      function timeHolder() {
-        clearInterval(clock);
-        clock = setInterval(seconds, 1000);
-        function seconds() {
-            if (timer === 0) {
-            clearInterval(clock);
-            unanswered();
-          } else if (timer > 0) {
-            timer--;
-            $('.time').html(timer);
-          }
-        }
-      }
-      
-      // Append reset button, correct, incorrect, and unanswered questions upon completing the trivia
-      function finishTrivia() {
-        var final = $('.main')
-          .html("<p>Here's your results!<p><br>")
-          .append('<p>Correct Answers: ' + correctCounter + '</p><br>')
-          .append('<p>Wrong Answers: ' + incorrectCounter + '</p><br>')
-          .append('<p>Unanswered Answers: ' + unansweredCounter + '</p><br>');
-        $(final).attr('<div>');
-        $(final).attr('class', 'final');
-        $('.final').append('<p><a class="reset-button" href="#">Restart Trivia!</a></p>');
-      }
